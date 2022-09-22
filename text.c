79b3acb5445af99143b7293d8cf501ba4511a94b
Kowalski Dragon (kowalski7cc) <kowalski7cc@users.noreply.github.com>
2022-09-22 21:12:31 +0200

    Add file picker
    Signed-off-by: Kowalski Dragon (kowalski7cc) <kowalski7cc@users.noreply.github.com>

--- text.c
+++ text.c
@@ -1,14 +1,36 @@
 #include <furi.h>
 #include "text.h"
 
-int32_t text_reader_app(void* p) {
-    TextApp* text_app = malloc(sizeof(TextApp));
+int32_t text_reader_app(void *p)
+{
+    TextApp *text_app = malloc(sizeof(TextApp));
 
-    if(p) {
-        // Open file by path
-    } else {
-        // Show file browser
+    string_t file_path;
+    string_init(file_path);
+
+    if (p)
+    {
+        string_cat_str(file_path, p);
+    }
+    else
+    {
+        string_set_str(file_path, TEXT_READER_APP_PATH_FOLDER);
+
+        DialogsFileBrowserOptions browser_options;
+        dialog_file_browser_set_basic_options(
+            &browser_options, TEXT_READER_APP_EXTENSION, &I_music_10px);
+        browser_options.hide_ext = false;
+        DialogsApp *dialogs = furi_record_open(RECORD_DIALOGS);
+        bool res = dialog_file_browser_show(dialogs, file_path, file_path, &browser_options);
+
+        furi_record_close(RECORD_DIALOGS);
+        if (!res)
+        {
+            FURI_LOG_E(TAG, "No file selected");
+            break;
+        }
     }
 
+    string_clear(file_path);
     free(text_app);
 }
--- text.h
+++ text.h
@@ -1,9 +1,13 @@
 #pragma once
+#include <text_files.h>
+#include <dialogs/dialogs.h>
+#include <storage/storage.h>
 #include <gui/gui.h>
 #include <gui/view_dispatcher.h>
 #include <input/input.h>
 
-typedef struct {
-    Gui* gui;
-    ViewDispatcher* view_dispatcher
+typedef struct
+{
+    Gui *gui;
+    ViewDispatcher *view_dispatcher
 } TextApp;
